9f304b18b8bda702acd079dd4df99ed4fb303403
koteyka121991 <106138744+koteyka121991@users.noreply.github.com>
2022-05-29 16:15:04 +0300

    add  tab navigation

--- js/js.js
+++ js/js.js
@@ -110,17 +110,32 @@ menuBurger.addEventListener('click', () => {
 
 
 
-const tabTechnicalSpecifications = document.querySelector('.tab-technical-specifications'),
-    tabDescription = document.querySelector('.tab-description'),
-    specification = document.querySelector('.description'),
-    technicalSpecifications = document.querySelector('.technical-specifications');
+const tabsBtn = document.querySelectorAll('.tab-description'),
+tabsItem = document.querySelectorAll('.tab-item');
+
+tabsBtn.forEach(oneTabClick);
 
 
-tabTechnicalSpecifications.addEventListener('click', () => {
-    technicalSpecifications.classList.toggle('active');
-})
+function oneTabClick (item) {
+    item.addEventListener('click', () => {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute('data-tab');       
+       let currentTab = document.querySelector(tabId);
+       if (!currentBtn.classList.contains('active')){
+        tabsBtn.forEach((item) => {                          
+            item.classList.remove('active');
+        })
+        tabsItem.forEach((item) => {                          
+            item.classList.remove('active');
+        })
+      }
+            currentBtn.classList.add ('active');
+            currentTab.classList.add ('active');
+    });
+}
+document.querySelector('.tab-description').click();
 
-tabDescription.addEventListener('click', () => {
-    specification.classList.toggle('active');
 
-})
+
+// poput menu
+
